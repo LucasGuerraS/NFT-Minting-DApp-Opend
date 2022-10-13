9cf22e07ea2d5e15340595e8bf07c47205a73512
LucasGuerraS <lucasgasardo@gmail.com>
2022-10-13 18:29:29 -0300

    🚧 Working on the base of the discover base RN

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,21 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import { Principal } from "@dfinity/principal";
+import Button from "./Button";
+import { opend } from "../../../declarations/opend";
 
-function Item() {
+function Item(props) {
+  const [name, setName] = useState();
+  const [owner, setOwner] = useState();
+  const [img, setImg] = useState();
+  const [button, setButton] = useState();
+  const [priceInput, setPriceInput] = useState();
+  const [loaderHidden, setLoader] = useState(true);
+  const [blur, setBlur] = useState();
+  const [sellStatus, setStatus] = useState("")
+
+  const id = props.id;
+
+  const localhost = "http://localhost:8080/";
+  const agent = new HttpAgent({ host: localhost });
+  //TODO: When deploy to live remove line 21
+  agent.fetchRootKey();
+  let NFTActor;
+
+  async function loadNFT() {
+    NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id,
+    });
+
+    const nome = await NFTActor.getName();
+    const dono = await NFTActor.getOwner();
+    const imageData = await NFTActor.getAsset();
+    const imageContent = new Uint8Array(imageData);
+    const image = URL.createObjectURL(
+      new Blob([imageContent.buffer], { type: "image/png" })
+    );
+    setName(nome);
+    setOwner(dono.toText());
+    setImg(image);
+
+    const nftIsListed = await opend.isListed(props.id);
+
+    if (nftIsListed) {
+      setOwner("OpenD");
+      setBlur({ filter: "blur(4px)" });
+      setStatus("#Listed")
+    } else {
+      setButton(<Button handleClick={handleSell} text={"Sell"} />);
+    }
+  }
+
+  useEffect(() => {
+    loadNFT();
+  }, []);
+  let price;
+  function handleSell() {
+    console.log("clicked sell");
+    setPriceInput(
+      <input
+        placeholder="Price in DLKZ"
+        type="number"
+        className="price-input"
+        value={price}
+        onChange={(e) => (price = e.target.value)}
+      />
+    );
+    setButton(<Button handleClick={sellItem} text={"Confirm"} />);
+  }
+
+  async function sellItem() {
+    setBlur({ filter: "blur(4px)" });
+    setLoader(false);
+    console.log("Set price = " + price);
+    const listingResult = await opend.listItem(props.id, Number(price));
+    console.log("listing: " + listingResult);
+    if (listingResult == "Success") {
+      const opendId = await opend.getOpenDCanisterID();
+      const transferResult = await NFTActor.transferOwnership(opendId);
+      console.log("transfer: " + transferResult);
+      if (transferResult == "Success") {
+        setLoader(true);
+        setButton();
+        setPriceInput();
+        setOwner("OpenD");
+        setStatus("#Listed")
+      }
+    }
+  }
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={img}
+          style={blur}
         />
+        <div hidden={loaderHidden} className="lds-ellipsis">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}
+            <span className="purple-text"> {sellStatus}</span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: {owner}
           </p>
+          {priceInput}
+          {button}
         </div>
       </div>
     </div>
